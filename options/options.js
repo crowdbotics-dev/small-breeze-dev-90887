ef7a0cb7f5a02957b7bdbc1e47198b7711fd7940
Crowdbotics <engineering@crowdbotics.com>
2023-06-12 23:09:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "small-breeze-dev-90887",
+  url: "https://small-breeze-dev-90887.botics.co",
+  api: "https://small-breeze-dev-90887.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
